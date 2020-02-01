5a1c034bd57fa6b1c1fd10fdf3ae218fe9925b4e
Anderson O. Aristides <anderson.aristides@fh.com.br>
2020-02-01 18:09:43 -0300

    Feat: First Commit

---
+++ codigoaberto/t1/source/Config.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * SITE CONFIG
+ */
+define("SITE", [
+    "name" => "Auth em MVC com PHP",
+    "desc" => "Aprenda a construir uma aplicação de autenticação em MVC com PHP do Jeito Certo",
+    "domain" => "localauth.com",
+    "locale" => "pt_BR",
+    "root" => "https://localhost/codigoaberto/t1"
+]);
+
+/**
+ * SITE MINIFY
+ */
+if ($_SERVER["SERVER_NAME"] == "localhost") {
+    require __DIR__ . "/Minify.php";
+}
+
+/**
+ * DATABASE CONNECT
+ */
+define("DATA_LAYER_CONFIG", [
+    "driver" => "mysql",
+    "host" => "localhost",
+    "port" => "3306",
+    "dbname" => "auth",
+    "username" => "root",
+    "passwd" => "salmos89",
+    "options" => [
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+        PDO::ATTR_CASE => PDO::CASE_NATURAL
+    ]
+]);
+
+/**
+ * SOCIAL CONFIG
+ */
+define("SOCIAL", [
+    "facebook_page" => "makersit",
+    "facebook_author" => "anderson.o.aristides",
+    "facebook_appId" => "",
+    "twitter_creator" => "@aoaristides",
+    "twitter_site" => "@aoaristides"
+]);
+
+/**
+ * MAIL CONNECT
+ */
+define("MAIL", []);
+
+/**
+ * SOCIAL LOGIN: FACEBOOK
+ */
+define("FACEBOOK_LOGIN", []);
+
+/**
+ * SOCIAL LOGIN: GOOGLE
+ */
+define("GOOGLE_LOGIN", []);
